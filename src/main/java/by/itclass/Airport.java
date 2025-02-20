233d24315a13d8ee25f05487ab01840806235a2c
Test User <Test5User@yandex.by>
2025-02-20 20:24:43 +0300

    Airport 1

---
+++ src/main/java/by/itclass/Airport.java
@@ -0,0 +1,11 @@
+package by.itclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airport {
+    public static void main(String[] args) {
+        SpringApplication.run(Airport.class);
+    }
+}
